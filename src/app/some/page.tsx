906aaefe99a10241679893388f9aedb956218ca9
Munkhzol Sainjargal <munhzol@gmail.com>
2023-08-26 12:56:50 -0400

    some fetchy

--- src/app/some/page.tsx
+++ src/app/some/page.tsx
@@ -1,3 +1,28 @@
-export default function SomePage() {
-  return <h1>Some Page</h1>;
+import { Todo } from "@/lib/types";
+
+async function getData(): Promise<Todo[]> {
+  const data = await fetch("https://dummyjson.com/todos", {
+    cache: "no-store",
+  });
+
+  const ret = await data.json();
+
+  console.log(ret.todos);
+
+  return ret.todos;
+}
+
+export default async function SomePage() {
+  const todos = await getData();
+
+  return (
+    <h1>
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.todo}</li>;
+        })}
+        <li></li>
+      </ul>
+    </h1>
+  );
 }
